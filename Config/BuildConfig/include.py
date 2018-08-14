9a530ea26d5f56504b6d1d783603eb34a4401e97
Jason Zhou <jason.zhou@veoneer.com>
2018-08-14 23:53:32 +0800

    fix some issue

--- Config/BuildConfig/include.py
+++ Config/BuildConfig/include.py
@@ -2,10 +2,11 @@
 
 # Include path
 BOOT_IncludePath = [
-   # '#Source\\Generic\\Driver\\Mcu\\Config\\CMSIS\\',
-   # '#Source\\Generic\\Driver\\Mcu\\Libraries\\CMSIS\\Include\\',
-   # '#Source\\Generic\\Driver\\Mcu\\Libraries\\CMSIS\\Device\\ST\\STM32F4xx\\Include\\',
-   # '#Source\\Generic\\Driver\\Mcu\\Libraries\\STM32F4xx_StdPeriph_Driver\\inc\\',
+   '#Source\\Generic\\Driver\\Mcu\\config\\',
+   '#Source\\Generic\\Driver\\Mcu\\Libraries\\CMSIS\\Include\\',
+   '#Source\\Generic\\Driver\\Mcu\\Libraries\\CMSIS\\Device\\ST\\STM32F4xx\\Include\\',
+   '#Source\\Generic\\Driver\\Mcu\\Libraries\\STM32F4xx_StdPeriph_Driver\\inc\\',
+   '#Source\\Generic\\Support\\mcu_common\\',
    
    ]
 APP_IncludePath = BOOT_IncludePath
